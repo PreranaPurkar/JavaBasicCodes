d4153399af6dc89e63868f96688f274e95cfd128
PreranaPurkar <preranapurkar12@gmail.com>
2023-04-03 22:01:13 +0530

    [ADDED]:[Prerana] print starting to end number

---
+++ Startn.java
@@ -0,0 +1,14 @@
+//Write a program to print starting number to end
+
+class Startn
+{
+	public static void main(String args[])
+	{
+		int n,i;
+		n=Integer.parseInt(args[0]);
+		for(i=0;i<=n;i++)
+		{
+		System.out.println(i);
+		}
+	}
+}
