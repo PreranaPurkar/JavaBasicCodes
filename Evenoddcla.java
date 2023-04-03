97dae417e9b869ead20e04079f46b7d12da6900a
PreranaPurkar <preranapurkar12@gmail.com>
2023-04-03 16:49:25 +0530

    [ADDED]:[Prerana] Find the Even or Odd number

---
+++ Evenoddcla.java
@@ -0,0 +1,18 @@
+//Write a program for Even or Odd Number
+class Evenoddcla
+{
+	public static void main(String args[])
+	{
+		int n;
+		n=Integer.parseInt(args[0]);
+		if(n%2==0)
+		{
+			System.out.println("Even number");
+		}
+		else
+		{
+			System.out.println("Odd number");
+
+		}
+	}
+}
