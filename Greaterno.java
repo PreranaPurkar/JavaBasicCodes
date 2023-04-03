013d9fbfb0f34116e26876fc7bb9bd1935174cba
PreranaPurkar <preranapurkar12@gmail.com>
2023-04-03 17:31:01 +0530

    [ADDED]:[Prerana] Greater number

---
+++ Greaterno.java
@@ -0,0 +1,19 @@
+//Write a program to find greater number
+
+class Greaterno
+{
+	public static void main(String args[])
+	{
+		int a,b;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		if(a>b)
+		{
+			System.out.println("A is Greater");
+		}
+		else
+		{
+			System.out.println("B is Greater");
+		}
+	}
+}
